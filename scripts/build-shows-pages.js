0533b451c021c83c1cb8d3f0d205e5e8b6b0c19a
Zahra Yousofi <zahra.yousofi.2010@gmail.com>
2021-12-04 10:18:32 -0500

    dom

--- scripts/build-shows-pages.js
+++ scripts/build-shows-pages.js
@@ -1,18 +1,37 @@
-
-document.querySelector('body');
-const bodyElement =document.querySelector('body');
+/*document.querySelector('body')
+const bodyElement = document.querySelector('body')
 const h1Element = document.createElement('h1')
 bodyElement.appendChild(h1Element)
 bodyElement.appendChild(document.createElement('h1'))
-const textNode =document.createTextNode('Mon Sept 06 2021')
+bodyElement.appendChild(document.createElement('h1'))
+const textNode = document.createTextNode('DATE')
+bodyElement.appendChild(textNode)
+const textNode = document.createTextNode('VENUE')
+bodyElement.appendChild(textNode)
+const textNode = document.createTextNode('LOCATION')
+bodyElement.appendChild(textNode)*/
+// console.log(newDiv);
+// var newDivText = document.createTextNode('DATE');
+// newDiv.appendChild(newDivText);
 
-const text =document.querySelector(".shows")
+// var container = document.querySelector('header .container');
+
+// var h1 = document.querySelector('header h1');
+// console.log(newDiv);
+// container.insertBefore(newDiv, h1);
 
 
 
+var div = document.createElement('div');
+div.className ="concert"
 
+var p = document.createElement('p');
+p.textContent = 'San Francisco';
 
-for(let i=0; i<6; i++) {
-    console.log('San Francisco, CA', 1);
-}
+div.appendChild(p);
+console.log(div);
+
+var container = document.getElementById('container');
+container.appendChild(div);
+
 
--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,17 +1,30 @@
 const name = document.getElementById('name')
 const comment = document.getElementById('comment')
+const submit = document.getElementById('submit')
 const form = document.getElementById('form')
 const errorElement = document.getElementById('error')
 
-form.addElementListener('comment', (e) => {
-    let messages =[]
-    if (name.value === '' || name.value == null) {
-        messages.push('Name is required')
-    }
-    if (messages.length > 0) {
-        e.preventDefault()
-        errorElement.innerText = messages.join(', ')
-    }
-})
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    console.log(event.target.name.value);
+    console.log(event.target.comment.value);
+    console.log(event.target.submit.value);
+});
+}
 
 
+
+
+/*document.querySelector('shows');
+const bodyElement =document.querySelector('shows');
+const h1Element = document.createElement('h1')
+bodyElement.appendChild(h1Element)
+bodyElement.appendChild(document.createElement('h1'))
+const textNode =document.createTextNode('Mon Sept 06 2021')
+
+const text =document.querySelector(".shows")
+
+for(let i=0; i<6; i++) {
+    console.log('San Francisco, CA', 1);
+}*/
