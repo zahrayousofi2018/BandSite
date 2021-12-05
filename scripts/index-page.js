356c909751cc9278b33743b875fee08b15b29061
Zahra Yousofi <zahra.yousofi.2010@gmail.com>
2021-12-05 17:57:54 -0500

    bio-page

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,41 +1,40 @@
 
-/*const name = document.getElementById('name')
-const comment = document.getElementById('comment')
-const submit = document.getElementById('submit')
-const form = document.getElementById('form')
-const errorElement = document.getElementById('error')
-
-
-form.addEventListener('click', function(event) {
-    event.preventDefault();
-    console.log(event.target.name.value);
-    console.log(event.target.comment.value);
-    console.log(event.target.submit.value);
-}) */
-
 const submitButton =document.querySelector('#submit');
-
-// const form = document.querySelector('#form')
-// const errorMessageElement = document.createElement('p');
-// errorMessageElement.innerHTML ='your comment is too short';
-
-submitButton.addEventListner('click', (event) => {
+const formContainerDiv =document.querySelector('#formContainer');
+const errorMessageElement = document.createElement('p');
+errorMessageElement.innerHTML ='your comment is too short';
+submitButton.addEventListener('click', (event) => {
     event.preventDefault();
-    const textFromInput = document.querySelector('#name').value;
-    if (textFromInput.length <10) {
+
+    const textFromInput = document.querySelector('#input').value;
+    // const textFromInput =document.querySelector('#opinion').value;
+    if (textFromInput.length < 10) {
         const formElement = document.querySelector('#form');
-         formElement.classList.add('error');
-        formDiv.appendChild(errorMessageElement);
-         return;
+        formElement.classList.add('error');
+        formContainerDiv.appendChild(errorMessageElement);
+        return;
     }
      const commentElement = document.createElement('p');
      commentElement.innerText = textFromInput;
+    //  commentElement.innerText = textFromOpinion;
      const commentSection = document.querySelector('#comments');
-     const textFromInput = document.querySelector('#submit').value;
-     commentSection.appendChild(commentElement)
-    form.removeChild(errorMessageElement)
+     commentSection.appendChild(commentElement);
+    //  formContainerDiv.removeChild(errorMessageElement)
     
 })
+
+     const textFromOpinion = document.querySelector('#opinion').value;
+    if(textFromOpinion.length <20) {
+        const formElement = document.querySelector('#form');
+        formElement.classList.add('error');
+        formContainerDiv.appendChild(errorMessageElement);
+        // return;
+}
+    const commentElement = document.createElement('p');
+    commentElement.innerText =textFromOpinion;
+    const commentSection =document.querySelector('#comments');
+    commentSection.appendChild(commentElement);
+
 
 
 
