ed64698f5f5328ce99519d572ce69c1834fe4437
Zahra Yousofi <zahra.yousofi.2010@gmail.com>
2021-12-13 20:56:54 -0500

    sprint-3

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -2,7 +2,6 @@
 const formElement =document.querySelector('#form');
 const formContainerDiv =document.querySelector('#formContainer');
 const errorMessageElement = document.createElement('p');
-// errorMessageElement.innerHTML ='your comment is too short';
 form.addEventListener('submit', (event) => {
     event.preventDefault();
     const commentSection = document.querySelector('#comments');
@@ -10,7 +9,6 @@ form.addEventListener('submit', (event) => {
     const textFromInput = event.target.input.value;
     const textFromOpinion = event.target.opinion.value;
     if (textFromInput.length < 10) {
-        // const formElement = document.querySelector('#form');
         formElement.classList.add('error');
         formContainerDiv.appendChild(errorMessageElement);
         return;
@@ -19,36 +17,30 @@ form.addEventListener('submit', (event) => {
      const nameElement = document.createElement('h4');
      nameElement.classList.add('form__heading');
      commentElement.classList.add('form__text');
-    //commentElement.getElementByID.add('form__comment__image');
      commentElement.innerText = textFromOpinion;
      nameElement.innerText = textFromInput;
      commentSection.appendChild(nameElement);
      commentSection.appendChild(commentElement);
      
 
-
-    //  const textFromOpinion = document.querySelector('#opinion').value;
     if(textFromOpinion.length <20) {
-        // const formElement = document.querySelector('#form');
+
         formElement.classList.add('error');
         formContainerDiv.appendChild(errorMessageElement);
 }
-    // const commentElement = document.createElement('p');
-    // commentElement.innerText = textFromOpinion; 
-    // commentSection.appendChild(commentElement);
-})
-
-axios
-    .get('https://project-1-api.herokuapp.com/')
-    .then((result) => {
-    console.log(result.data);
 });
 
 
+axios.get('https://project-1-api.herokuapp.com/').then(response => {
+  console.log(response.data);
+}
+);
 
+const Http = new XMLHttpRequest();
+const url='/register';
+Http.responseXML("GET", url);
+Http.send();
 
-
-
-
-
-
+Http.onreadystatechange =(e)=> {
+    console.log(Http.responseText)
+}
